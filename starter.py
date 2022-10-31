890775f1e443499a3daa3a977beb9df7ca2eaf1e
Sniggiho <rohiggin@macalester.edu>
2022-10-30 19:37:48 -0500

    PuLP working on example

--- starter.py
+++ starter.py
@@ -1,9 +1,9 @@
 from pulp import LpMaximize, LpProblem, LpStatus, LpVariable
 
-model = LpProblem(name="test", sense=LpMaximize)
+model = LpProblem(name='test', sense=LpMaximize)
 
 x = LpVariable(name="x", lowBound=0, upBound=100)
-y = LpVariable(name="x", lowBound=0, upBound=100)
+y = LpVariable(name="y", lowBound=0, upBound=100)
 
 model += (x<=95)
 model += (y<=95)
@@ -13,4 +13,7 @@ model += obj_func
 
 status = model.solve()
 
-# print(status)
+print(f"status: {model.status}, {LpStatus[model.status]}")
+print(f"objective: {model.objective.value()}")
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
