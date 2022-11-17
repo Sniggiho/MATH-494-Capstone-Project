dcfa64bf7c94a41e0dff7c832dc67cfc1aece2b1
Sniggiho <rohiggin@macalester.edu>
2022-11-17 16:54:09 -0600

    Added David

--- ClassesLP.py
+++ ClassesLP.py
@@ -8,16 +8,16 @@ import csv # csv reading
 
 # --------------------------- import data and create V matrix -----------------------------
 
-profs = ["Alireza", "Andrew", "Kristin", "Lisa", "Lori", "Racheal", "Taryn", "Will"] # the names of each professor to be included in the program
+profs = ["Alireza", "Andrew", "David", "Kristin", "Lisa", "Lori", "Racheal", "Taryn", "Will"] # the names of each professor to be included in the program
 
 vMat = [None]*len(profs) # initialize the V matrix to be empty
 
 i = 0 # loop variable that numbers the profs
 
-for prof in profs: # reads in each CSV, adding its data to vMat
+for prof in profs: # reads in each CSV, adding its data to vMat. Numbers professors in the order they appear in profs
     currPath = 'V_pai CSVs by Prof\V_' + prof + '.csv'
     with open(currPath, newline = '') as csvfile:
         vMat[i] = list(csv.reader(csvfile))
     i += 1
-    
+
 #------------------------------------------------------------------------------------------
