f7be9791b26c84295d9add15e55439fcc7ab4595
Sniggiho <rohiggin@macalester.edu>
2022-11-22 11:17:27 -0600

    constraints 1,2,4 added

--- ClassesLP.py
+++ ClassesLP.py
@@ -45,13 +45,13 @@ def makeWMat(listOfCourseNumbers):
         400400: 3   
     }
 
-    wMat = [[0 for x in range(len(demoClasses))] for y in range(len(demoClasses))] # initializes the weight matrix with all 0s
+    wMat = [[0 for x in range(len(listOfCourseNumbers))] for y in range(len(listOfCourseNumbers))] # initializes the weight matrix with all 0s
 
-    for a in range(len(demoClasses)): # TODO: super inelegant!!! exploit matrix symmetry
-        for b in range(len(demoClasses)):
-            aLevel = demoClasses[a] - demoClasses[a]%100
+    for a in range(len(listOfCourseNumbers)): # TODO: super inelegant!!! exploit matrix symmetry
+        for b in range(len(listOfCourseNumbers)):
+            aLevel = listOfCourseNumbers[a] - listOfCourseNumbers[a]%100
 
-            bLevel = demoClasses[b] - demoClasses[b]%100
+            bLevel = listOfCourseNumbers[b] - listOfCourseNumbers[b]%100
             key1 = 1000*aLevel + bLevel
             key2 = 1000*bLevel + aLevel
 
@@ -67,30 +67,52 @@ def makeWMat(listOfCourseNumbers):
 
 
 # -------------------------------- the LP !!???!?!?! ---------------------------------------
+profsNumerical = list(range(len(profs)))
+
 courses = [135,137,236,237,279,375,377,479] # a random assortment of courses
-profs
+coursesNumerical = list(range(len(courses)))
+
 intervals = list(range(12))
+
 
 
 model = LpProblem(name='classes', sense=LpMinimize)
 
-x = LpVariable.dicts("x", (profs, courses, intervals), cat="Binary")
+x = LpVariable.dicts("x", (profsNumerical, coursesNumerical, intervals), cat="Binary")
+
+
+# CONSTRAINT 1:
+for a in coursesNumerical:
+    model += lpSum(x[p][a][i] for p in profsNumerical for i in intervals) == 1
+
+# CONSTRAINT 2:
+for p in profsNumerical:
+    for i in intervals:
+        model += lpSum(x[p][a][i] for a in coursesNumerical) <= 1
+
+# # CONSTRAINT 3:
+# for p in profsNumerical:
+#     for a in coursesNumerical:
+#         for i in intervals:
+#             print(vMat[p][a][i])
+#             print(x[p][a][i])
+
+#             model += lpSum(vMat[p][a][i] - x[p][a][i]) >= 0
+
+
+# CONSTRAINT 4:
+for p in profsNumerical:
+    model += lpSum(x[p][a][i] for a in coursesNumerical for i in intervals) <= 3
 
 
 
-# x = LpVariable(name="x", lowBound=0, upBound=100)
-# y = LpVariable(name="y", lowBound=0, upBound=100)
+obj_func = 0
+model += obj_func
 
-# model += (x<=95.5)
-# model += (y<=94.5)
+status = model.solve()
 
-# obj_func = x + y
-# model += obj_func
+print(f"objective: {model.objective.value()}")
 
-# status = model.solve()
-
-# print(f"objective: {model.objective.value()}")
-
-# print(f"status: {model.status}, {LpStatus[model.status]}")
-# for var in model.variables():
-#     print(f"{var.name}: {var.value()}")
+print(f"status: {model.status}, {LpStatus[model.status]}")
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
--- starter.py
+++ starter.py
@@ -1,18 +1,15 @@
-from pulp import LpMaximize, LpProblem, LpStatus, LpVariable
+from pulp import*
 
-model = LpProblem(name='test', sense=LpMaximize)
+# model = LpProblem(name='test', sense=LpMaximize)
 
-x = LpVariable(name="x", lowBound=0, upBound=100)
-y = LpVariable(name="y", lowBound=0, upBound=100)
+# x = LpVariable(name="x", lowBound=0, upBound=100)
+# y = LpVariable(name="y", lowBound=0, upBound=100)
 
-for var in model.variables():
-    print(f"{var.name}")
+# model += (x<=95)
+# model += (y<=95)
 
-model += (x<=95)
-model += (y<=95)
-
-obj_func = x + y
-model += obj_func
+# obj_func = x + y
+# model += obj_func
 
 # status = model.solve()
 
@@ -20,3 +17,34 @@ model += obj_func
 # print(f"objective: {model.objective.value()}")
 # for var in model.variables():
 #     print(f"{var.name}: {var.value()}")
+
+
+model = LpProblem(name='classes', sense=LpMaximize)
+
+dummy = ['a','b']
+dummy2 = [1,2,3]
+dummy3 = [135,237]
+
+x = LpVariable.dicts("x", (dummy, dummy2, dummy3), lowBound=10, upBound=100)
+
+# x = LpVariable(name="x", lowBound=0, upBound=100)
+# y = LpVariable(name="y", lowBound=0, upBound=100)
+
+
+# sum of x_a_i == 90 over i for all a
+
+for c in dummy:
+    model += lpSum(x[c][i][num] for i in dummy2 for num in dummy3) == 90
+
+
+
+obj_func = x['a'][1][135] + x['b'][2][237] + x['b'][1][135] +x['a'][2][237]
+model += obj_func
+
+status = model.solve()
+
+print(f"objective: {model.objective.value()}")
+
+print(f"status: {model.status}, {LpStatus[model.status]}")
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
